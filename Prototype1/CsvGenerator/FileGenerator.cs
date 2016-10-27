602f19b32e3f2cf60d670327eb5ca476dc685602
aakizu <aakizu@DESKTOP-EBP284B>
2016-10-27 10:13:27 +0200

    Generador de CSV creado

---
+++ Prototype1/CsvGenerator/FileGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CsvGenerator
+{
+    public class FileGenerator
+    {
+        public FileGenerator(string _referencePath, string _destinationPath)
+        {
+            referencePath = _referencePath;
+            destinationPath = _destinationPath;
+        }
+
+        public List<string> getCsvFileNames()
+        {
+            var fileList = Directory.GetFiles(referencePath, "*.csv");
+
+            List<string> fileNames = new List<string>();
+            foreach (var item in fileList)
+            {
+                string tempName = Path.GetFileName(item);
+                fileNames.Add(tempName);
+            }
+
+            return fileNames;
+        }
+
+        public void GenerateCsv(string referencePath, string destinationPath, string fileName)
+        {
+            File.Copy(referencePath + fileName, destinationPath + fileName, true);
+            Console.WriteLine("Created {0} file", fileName);
+        }
+
+        private string referencePath { get; set; }
+        private string destinationPath { get; set; }
+    }
+}
---
+++ Prototype1/CsvGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CsvGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                MainFunction obj = new MainFunction(args[0], args[1]);
+                obj.StartTimer();
+            }
+        }
+
+    }
+}
