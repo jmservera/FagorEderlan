c9dbaa6ce96df6a8016b6c47529b3844f32203a0
juanserv <juanserv@DESKTOP-K6ICE1B>
2016-10-28 18:49:25 +0200

    depoyment and query with all the data

--- Prototype1/asaFileAnalyzer.sql
+++ Prototype1/asaFileAnalyzer.sql
@@ -1,6 +1,17 @@
-﻿SELECT
-    REF,TRY_CAST([tiempo1Carrera] as float) tiempo, TRY_CAST([Diferenciadepresion] as float) difpres,
-    DATEADD(ms,TRY_CAST([tiempo1Carrera] as float), EventProcessedUtcTime) timestamp_t,  *
+﻿with curvas as (SELECT
+    REF,TRY_CAST(tiempo1Carrera as float) tiempo,
+    TRY_CAST(Diferenciadepresion as float) difpres,
+    DATEADD(ms,TRY_CAST(tiempo1Carrera as float), EventProcessedUtcTime) timestamp_t,
+    TRY_CAST(presionp2 as float) presionp2,
+    TRY_CAST(contrapresion as float) contrapresion,
+    TRY_CAST(canal5 as float) canal5,
+    TRY_CAST(canal6 as float) canal6,
+    TRY_CAST(canal7 as float) canal7,
+    TRY_CAST(canal8 as float) canal8,
+    TRY_CAST(presionp1 as float) presionp1
 FROM
     input
---where [tiempo1Carrera]='1.0'
+timestamp by DATEADD(ms,TRY_CAST(tiempo1Carrera as float), EventProcessedUtcTime)
+    )
+select * from curvas
+--where tiempo<2;
