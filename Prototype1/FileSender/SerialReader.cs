6adf8c87b3739c48cc5154aaed54d1bdc5c87132
Juan Manuel Servera <jmservera@outlook.com>
2016-11-07 00:44:42 +0100

    remove unused

--- Prototype1/FileSender/SerialReader.cs
+++ Prototype1/FileSender/SerialReader.cs
@@ -40,19 +40,7 @@ namespace FileSender
         private async void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             var value=serialPort.ReadLine();
-            //byte[] data = new byte[serialPort.BytesToRead];
-            //int lenght=serialPort.Read(data, 0, data.Length);
-            //var value=Encoding.ASCII.GetString(data);
             await hub.SendDataAsync(value);
-        }
-
-        void processData()
-        {
-            // Determine if we have a "packet" in the queue
-            if (recievedData.Count > 50)
-            {
-                var packet = Enumerable.Range(0, 50).Select(i => recievedData.Dequeue());
-            }
         }
 
         public void Dispose()
